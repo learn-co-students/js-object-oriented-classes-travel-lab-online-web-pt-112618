a99f93338c1c09963e1ca25da4a06e48ac262cad
github username <github email address>
2019-08-03 16:16:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  avenueIndex(avenue) {
+    return eastWest.indexOf(avenue)
+  }
+  blocksTravelled() {
+    let verticalDistance =  Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)//calculates the number of blocksTravelled
+    let horizonalDistance = Math.abs(this.avenueIndex(this.beginningLocation.horizontal) - this.avenueIndex(this.endingLocation.horizontal))//calculates the number of horizontal blocks travelled
+    return verticalDistance + horizonalDistance
+  }
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+
+  }
+
+}
